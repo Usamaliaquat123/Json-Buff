e8d74f10149340d7251c0f9ce6e3ae342a742950
Usamaliaquat123 <inconnent4@gmail.com>
2020-02-07 11:53:32 -0800

    handled undefined, string

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+// support stringify
+exports.stringify = function stringify(o) {
+    if ('undefined' == typeof o ) return o
 
+    if(o && Buffer.isBuffer(o)){
+        return JSON.stringify(':base64:' + o.toString('base64'))
+    }
+    if(o && o.toJSON())
+        o = o.toJSON()
+
+    if(o && 'object' === typeof o ){
+        // not handle object 
+    }else if('string' === typeof o){
+        return JSON.stringify(/^:/.test(o) ? ':' + o : o)
+    }else if('undefined' === typeof o ){
+        return null
+    }else{
+        return JSON.stringify(o)
+    }
+}
 
 // support parse
 exports.parse = function(s){
